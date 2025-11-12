39160a5c1900293f773a83e7d262d89ddb4a9cdf
Von <s.zajac@o2.pl>
2025-11-12 20:45:37 +0100

    Hotfix3, because I can't read...

--- populate_database.py
+++ populate_database.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.document import Document
+from langchain_core.documents import Document
 from get_embedding_function import get_embedding_function
 from langchain_chroma import Chroma
 
